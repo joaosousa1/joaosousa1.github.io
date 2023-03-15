fdc45cc67cf8fae9cead54d2799a0a6fdd6413f3
João Sousa <tuxfire@gmail.com>
2023-03-15 01:09:34 +0000

    new style

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
-// copyright João Sousa 2022 //
-setTimeout(() => {
-    document.body.style.opacity = "1";
-}, 500);
+// copyright João Sousa 2023 //
+
+setTimeout(() => { // loading...
+  try {
+    initIntersectionObserver();
+  } catch (error) {
+    console.log("página sem intersectionObserver");
+  }
+  document.body.style.opacity = "1";
+}, 300);
 
 //3d stuff
 var scene, cube
@@ -14,9 +20,7 @@ function init() {
 
     createRenderer();
     createCamera();
-    createLights();
-
-
+    
     fundo = addSkyBox("zeus");
 
     window.addEventListener('resize', onWindowResize, false);
@@ -29,7 +33,6 @@ function createCamera() {
     camera.position.y = 0;
     camera.position.x = 100;
     camera.lookAt(scene.position);
-
 }
 
 function createRenderer() {
@@ -37,7 +40,6 @@ function createRenderer() {
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
-
 }
 
 function createCube(size, x, y, z, image = null) { //tamanho cordenadas e textura
@@ -47,49 +49,18 @@ function createCube(size, x, y, z, image = null) { //tamanho cordenadas e textur
       var cube = new THREE.Mesh(new THREE.BoxGeometry(size, size, size), new THREE.MeshBasicMaterial({ map: jtexture }));
       cube.position.set(x, y, z);
       scene.add(cube);
-      return cube;
     } else {
       var geometry = new THREE.BoxGeometry(size, size, size);
       var material = new THREE.MeshLambertMaterial({ color: Math.random() * 0xffffff });
       var cube = new THREE.Mesh(geometry, material);
       cube.position.set(x, y, z);
       scene.add(cube);
-      return cube;
     }
-      
-  }
-
-  function createLights() {
-    //set to true to view light positions
-    var helpers = false;
-  
-    light1 = new THREE.DirectionalLight(0xffffff, 1);
-    light1.position.set(5, 5, 30).normalize();
-    scene.add(light1);
-  
-    //Add helper to view light positionse	
-    if (helpers) {
-      light1.helper = new THREE.DirectionalLightHelper(light1, 50);
-      scene.add(light1.helper);
-    }
-  
-    light2 = new THREE.DirectionalLight(0xffffff, 1);
-    light2.position.set(-1, -1, -5).normalize();
-    scene.add(light2);
-  
-    //Add helper to view light position
-    if (helpers) {
-      light2.helper = new THREE.DirectionalLightHelper(light2, 50);
-      scene.add(light2.helper);
-    }
-  
-    return true;
   }
 
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
@@ -98,27 +69,11 @@ function animate() {
 
     renderer.render(scene, camera);
 
-
     fundo.rotation.y += 0.002;
-
-
-    // cube.rotation.x += -0.007;
-    // cube.rotation.y += -0.007;
-    // cube.rotation.z += -0.007;
-
-
-    // cube1.rotation.y += -0.007;
-    // cube2.rotation.y += -0.007;
-    // cube3.rotation.y += -0.007;
-    // cube1.rotation.z += -0.007;
-    // cube2.rotation.z += -0.007;
-    // cube3.rotation.z += -0.007;
 
 }
 
 function addSkyBox(nome) {
-    //skybox
-
     var ext = "jpg"
 
     let materialArray = [];
@@ -159,3 +114,12 @@ const cbox = document.querySelectorAll(".uiBotao");
     cbox[i].parentElement.classList.toggle("painelHover");
     });
 }
+
+// window.onscroll = function () { scrollFunction() };
+// function scrollFunction() {
+// 	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+// 		topBar.classList.add("topGlass");
+// 	} else {
+//     topBar.classList.remove("topGlass");
+// 	}
+// }
--- js/intersectionObs.js
+++ js/intersectionObs.js
@@ -1,27 +1,54 @@
-// copyright João Sousa 2022 //
-const iobs = document.querySelectorAll("[data-anim]");
+// © João Sousa 2023 //
 
-const options = {
-    root: null,
-    threshold: 0.1,
-    rootMargin: "0px"
+function intersectionObserver() {
+	const iobs = document.querySelectorAll("[data-anim]");
+
+	const options = {
+		root: null,
+		threshold: 0.1,
+		rootMargin: "0px"
+	}
+
+	let observer = new IntersectionObserver((entries) => {
+		let delay = 0;
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				delay += 0.3;
+				entry.target.style.animation = `${entry.target.dataset.anim} 0.6s ${delay}s forwards cubic-bezier(0.18, 0.89, 0.43, 1.19)`;
+			}
+			else {
+				// comentar se quiser só na entrada
+				entry.target.style.animation = 'none';
+			}
+		})
+
+	}, options);
+
+	iobs.forEach(iob => {
+		iob.classList.add("hide")
+		observer.observe(iob)
+	})
 }
 
-let observer = new IntersectionObserver((entries) => {
-    let delay = 0;
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            delay += 0.3;
-            entry.target.style.animation = `${entry.target.dataset.anim} 0.6s ${delay}s forwards cubic-bezier(0.18, 0.89, 0.43, 1.19)`;
-        }
-        else {
-            // comentar se quiser só na entrada
-            entry.target.style.animation = 'none';
-        }
-    })
-}, options);
+function initIntersectionObserver() {
+	// Safari
+	let isSafariBrowser = () => navigator.userAgent.indexOf('Safari') > -1 && navigator.userAgent.indexOf('Chrome') <= -1
 
-iobs.forEach(iob => {
-    iob.classList.add("fade")
-    observer.observe(iob)
-})
+	//IntersectionObserver não está totalmente implementado no safari.
+	if (isSafariBrowser()) {
+		const iobs = document.querySelectorAll("[data-anim]");
+		iobs.forEach(iob => {
+			iob.classList.remove("hide");
+		})
+	} else {
+		try {
+			intersectionObserver()
+		} catch (error) {
+			const iobs = document.querySelectorAll("[data-anim]");
+			iobs.forEach(iob => {
+				iob.classList.remove("hide");
+			})
+
+		}
+	}
+}
